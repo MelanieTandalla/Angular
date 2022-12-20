9026ec8cf332ec179ba77eeb3ad38a64c0f7516d
ErickGuevara19 <ead.guevara@yavirac.edu.ec>
2022-12-19 19:22:02 -0500

    igualacion y deber

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,37 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProductComponent } from './pages/product/product.component';
 import { CategoryComponent } from './pages/category/category.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { NavComponent } from './shared/nav/nav.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { NavLeftComponent } from './shared/nav-left/nav-left.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NofoundpageComponent } from './noFoundPages/nofoundpage.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { NoFoundPagesComponent } from './no-found-pages.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PagesComponent,
-    DashboardComponent,
-    ProductComponent,
-    CategoryComponent,
-    HeaderComponent,
-    NavComponent,
-    FooterComponent,
-    NavLeftComponent,
-    LoginComponent,
-    RegisterComponent,
-    NofoundpageComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, PagesComponent, LoginComponent, RegisterComponent,DashboardComponent, ProductComponent, CategoryComponent, SidebarComponent, BreadcrumbsComponent, NoFoundPagesComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
